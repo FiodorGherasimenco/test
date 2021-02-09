f7e25116a811a643eb34a7b7c7dc34c2ffe631b9
F.G <fiodor@kidoz.net>
2021-02-09 21:22:35 +0200

    Render ranked words

--- client/src/containers/Rank.js
+++ client/src/containers/Rank.js
@@ -1,15 +1,45 @@
 import { connect } from "react-redux";
 
 const Rank = ({ words }) => {
+  let maxCounter = 0;
+  for (let key in words) {
+    if (maxCounter < words[key]) {
+      maxCounter = words[key];
+    }
+  }
+
+  const groups = [100, 75, 50, 25, 0];
+  const result = [];
+  for (let key in words) {
+    const wordPercantage = Math.ceil((words[key] * 100) / maxCounter);
+    for (let i = 0; i < groups.length; i++) {
+      if (wordPercantage >= groups[i]) {
+        if (result[i] == null) {
+          result[i] = [key];
+        } else {
+          result[i].push(key);
+        }
+        break;
+      }
+    }
+  }
+
   return (
     <ul>
-      {words &&
-        Object.entries(words).map(([word, counter], i) => (
-          <li key={i}>
-            {word} 
-            <strong>{counter}</strong>
+      {result.map((words, i) =>
+        words.map((word, j) => (
+          <li key={j}>
+            {word} -{" "}
+            <strong>
+              (
+              {Array(result.length - i)
+                .fill("*")
+                .join("")}
+              )
+            </strong>
           </li>
-        ))}
+        ))
+      )}
     </ul>
   );
 };
